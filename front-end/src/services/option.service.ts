47d5b69705933f222685484c78474a47ee457772
Audoux Nicolas <nicolas.audoux13@gmail.com>
2022-04-22 11:41:22 +0200

    optionService

---
+++ front-end/src/services/option.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Option} from "../models/option.model";
+import {ActivatedRoute} from "@angular/router";
+import {serverUrl} from "../configs/server.config";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OptionService {
+  option : Option;
+  userId : string;
+
+  constructor(private http: HttpClient, public route : ActivatedRoute) {
+  }
+
+  setOption(id: string): void {
+    const optionUrl = serverUrl + "/option/" + id;
+    this.http.get<Option>(optionUrl).subscribe((option) => {
+      this.option = option[0];
+      document.documentElement.style.setProperty("--font-size",this.option.fontSize +"px");
+      document.documentElement.style.setProperty("--gap-row","50px");
+      document.documentElement.style.setProperty("--gap-column","150px");
+
+    });
+  }
+
+}
