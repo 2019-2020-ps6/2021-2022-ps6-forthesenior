758429d42a51a5e4b2db044642ecd7acb9f146bf
MatisPrograms <matis.herrmann@etu.unice.fr>
2022-05-25 11:37:30 +0200

    Error page added back to connection button

--- front-end/src/app/utils/error/error.path.ts
+++ front-end/src/app/utils/error/error.path.ts
@@ -1,17 +1,22 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-connection',
   templateUrl: './error.path.html',
   styleUrls: ['./error.path.scss']
 })
-// tslint:disable-next-line:component-class-suffix
+
 export class ErrorPath implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
-    console.log("Error 404: Page Not Found")
+    console.log("Error 404: Page Not Found");
+  }
+
+  backToConnection() {
+    this.router.navigate(["/connection"]);
   }
 }
